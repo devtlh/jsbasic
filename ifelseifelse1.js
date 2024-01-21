e47f5e00b517fbca89756cabc9a4343a70329ae7
Talha Unal <devtlh01@gmail.com>
2024-01-21 06:17:14 +0300

    ifelseifelse demneme yapıldı

---
+++ ifelseifelse1.js
@@ -0,0 +1,10 @@
+var ort=Number(prompt("ortalama: "));
+if(ort<50)
+    document.write("kaldı!!!");
+else if(ort<=65)
+    document.write("orta");
+else if(ort<=80)
+    document.write("İyi");
+else
+    document.write("Pekiyi");
+
