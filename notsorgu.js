aa3bc8024c9217f3df8d50ea9c09c4d36d77da2a
Talha Unal <devtlh01@gmail.com>
2024-01-21 23:11:38 +0300

    geçti kaldı sorgusu

---
+++ notsorgu.js
@@ -0,0 +1,10 @@
+var n1,n2,ort;
+n1=Number(prompt("Vize: "));
+n2=Number(prompt("Final: "));
+ort=(n1+n2)/2
+if(ort<50){
+    alert("Ortalama= "+ort+" Kaldı ")
+}
+else{
+    alert("Ortalama= "+ort+" Geçti!!! ")
+}
