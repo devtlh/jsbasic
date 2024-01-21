c95549bd0282a5f2b3c6960f1b9241a14af1d94d
Talha Unal <devtlh01@gmail.com>
2024-01-21 06:31:00 +0300

    yas sorgu uygulaması yapıldı

---
+++ yassorgu.js
@@ -0,0 +1,11 @@
+var yas=Number(prompt("Yaş Giriniz:"));
+if(yas<2)
+    alert("Bebek");
+else if(yas<=12)
+    alert("Çocuk");
+else if(yas<=40)
+    alert("Genç");
+else if(yas<=60)
+    alert("Yetişkin");
+else
+    alert("Yaşlı");
