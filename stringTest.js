36ff3b813f4e1ac0e82e5cba7470ee1c74321484
Talha Unal <devtlh01@gmail.com>
2024-01-29 22:50:51 +0300

    string fonksiyonları denendi

---
+++ stringTest.js
@@ -0,0 +1,9 @@
+var ad="DevTalha";
+
+document.write(ad.length+"<br>");
+document.write(ad.toLowerCase()+"<br>");
+document.write(ad.toUpperCase()+"<br>");
+document.write(ad.bold()+"<br>");
+document.write(ad.italics()+"<br>");
+document.write(ad.charAt(4)+"<br>");
+document.write(ad.substring(3,5)+"<br>");
