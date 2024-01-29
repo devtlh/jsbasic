5bcb1387c4574b883f788ae0d65b3ffb826c3165
Talha Unal <devtlh01@gmail.com>
2024-01-29 03:33:08 +0300

    nesne oluşturuldu

---
+++ ntp.js
@@ -0,0 +1,14 @@
+class Uye{
+    constructor(uyeNo){
+        this.uyeNo=uyeNo;
+    }
+}
+
+//sınıftan nesne
+
+var uye1=new Uye(1);
+var uye2=new Uye(2);
+
+//uye numaralarını yazdır
+
+document.write("ÜYE 1 NO: "+uye1.uyeNo);
