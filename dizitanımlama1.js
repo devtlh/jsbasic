e3a93ced1714a667f25d8d3738ec435f351c3996
Talha Unal <devtlh01@gmail.com>
2024-01-30 05:38:15 +0300

    dizi oluşturuldu yazdırıldı

---
+++ dizitanımlama1.js
@@ -0,0 +1,12 @@
+var fener=["ahmet","mehmet","ali"];
+
+var hafta=new Array(7);
+hafta[0]="pazartesi";
+hafta[1]="salı";
+hafta[2]="çarşamba";
+hafta[3]="perşembe";
+hafta[4]="cuma";
+hafta[5]="cumartesi";
+hafta[6]="pazar";
+
+var rakamlar=new Array(0,1,2,3,4,5,6,7,8,9);
---
+++ diziyazdirma.js
@@ -0,0 +1,12 @@
+var hafta=new Array(7);
+
+hafta[0]="pazartesi";
+hafta[1]="salı";
+hafta[2]="çarşamba";
+hafta[3]="perşembe";
+hafta[4]="cuma";
+hafta[5]="cumartesi";
+hafta[6]="pazar";
+
+document.write(hafta+"<br><br><br>");
+document.write(hafta[4]+"<br><br>");
